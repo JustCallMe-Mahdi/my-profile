b1ebfbd0961c2d162793d4632787ce27b84bb180
Mahdi <moxis8.7stream@gmail.com>
2025-04-03 20:17:16 +0330

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByClassName("primary-nav").addEventListener("click" , function()
-{
-    const target = document.getElementsByClassName("ghost-nav");
-    target.classList.remove("ghost-nav") ;
-    target.classList.add("primary-nav");
-});
